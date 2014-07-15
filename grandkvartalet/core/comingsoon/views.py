9fb57780b99f0ea60b5a9396acf9b001bfbb0227
Martin Skow Røed <msroed@gmail.com>
2014-07-15 18:13:12 +0200

    fix

--- grandkvartalet/core/comingsoon/views.py
+++ grandkvartalet/core/comingsoon/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from .forms import SubscriberCreationForm
 
 def comingsoon(request):
-	return render(request, 'index.jade')
+	if 'subscribed' not in request.session.keys():
+		request.session['subscribed'] = False
+	if 'email' not in request.session.keys():
+		request.session['email'] = ''
+	if request.POST:
+		form = SubscriberCreationForm(request.POST)
+		if form.is_valid():
+			sub = form.save()
+			request.session['subscribed'] = True
+			request.session['email'] = sub.email
+		else:
+			print form.errors
+	return render(request, 'index.jade', {'subscribed': request.session['subscribed'], 'sub_email': request.session['email']})
+

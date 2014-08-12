e95a0b5d1e8f5fcadc468442ac7619deec1330b3
Martin Skow Røed <msroed@gmail.com>
2014-08-12 11:49:24 +0200

    added email

--- grandkvartalet/core/landing/views.py
+++ grandkvartalet/core/landing/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from grandkvartalet.app.contact.forms import ContactForm
+from django.core.mail import EmailMessage
 
 def landing_view(request):
 	if request.POST:
 		form = ContactForm(request.POST)
 		if form.is_valid():
-			form.save()
+			contact = form.save()
+			email = EmailMessage('Ny epost fra grandkvartalet.no', 'Navn: %s \r\n\r\nEmail: %s\r\n\r\n %s' % (contact.name, contact.email, contact.message), to=['fer@skc.no', 'jan@inter-eiendom.no'])
+			email.send()
 	return render(request, 'landing.jade')
 
 def display_pdf(request, fileurl):
--- grandkvartalet/settings.py
+++ grandkvartalet/settings.py
@@ -102,3 +102,10 @@ TEMPLATE_LOADERS = (
     )),
 )
 
+# EMAIL
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'grandkvartalet.web@gmail.com'
+EMAIL_HOST_PASSWORD = 'ASDQWE123'
+EMAIL_PORT = 587
+

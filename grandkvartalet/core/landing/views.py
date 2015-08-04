99eab05d9edfb43c178777f0d6c255b912eb7752
Martin Skow Røed <msroed@gmail.com>
2015-08-04 14:53:15 +0200

    Using Mandrill for sending emails

--- grandkvartalet/core/landing/views.py
+++ grandkvartalet/core/landing/views.py
@@ -1,16 +1,36 @@
+
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from grandkvartalet.app.contact.forms import ContactForm
 from django.core.mail import EmailMessage
+import mandrill
 
 def landing_view(request):
 	if request.POST:
 		form = ContactForm(request.POST)
 		if form.is_valid():
 			contact = form.save()
-			email = EmailMessage('Ny epost fra grandkvartalet.no', 'Navn: %s \r\n\r\nEmail: %s\r\n\r\n%s' % (contact.name, contact.email, contact.message), to=[ 'jan@inter-eiendom.no', 'fer@skc.no'])
-			email.send()
+			mandrill_client = mandrill.Mandrill('lqyeqHAZ1Fc5MN-j10g_ug')
+			message = {
+				'from_email': 'kontakt@grandkvartalet.no',
+				'from_name': 'Kontakt fra grandkvartalet.no',
+				'to': [
+					{
+						'email': 'fer@skc.no',
+						'name': 'Finn Erik Roeed',
+						'type': 'to',
+					},
+					{
+						'email': 'jan@inter-eiendom.no',
+						'name': 'Jan Hansen',
+						'type': 'to'
+					},
+				],
+
+				'text': 'Navn: {}\r\n\r\nEmail: {}\r\n\r\n{}'.format(contact.name, contact.email, contact.message)
+			}
+			result = mandrill_client.messages.send(message=message, async=True)
 	return render(request, 'landing.jade')
 
 def display_pdf(request, fileurl):
